2c9c8e2b90329f3ff94b4c693687910c414bc4e5
brian <riceb53@gmail.com>
2021-02-24 20:33:07 -0500

    add guess a number game and name route

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -4,4 +4,35 @@ class Api::ParamsExamplesController < ApplicationController
     @output_message = "Here is the evil version of your name #{params[:first_name].upcase.reverse}"
     render 'query.json.jb'
   end
+
+  def name
+    # # take in username
+    # p params[:name]
+    # # make all caps
+    # p params[:name].upcase
+    # send back to user
+    @name = params[:name].upcase
+    if @name[0] == "A"
+      @message = "Your name begins with A"
+    else
+      @message = "I don't have a message for you..."
+    end
+
+    render 'name.json.jb'
+  end
+
+  def guess
+    # take in user input
+    @user_guess = params[:guess].to_i
+    # figure out if it's the right number
+    if @user_guess > 35
+      @message = "you guessed too high"
+    elsif @user_guess < 35
+      @message = "you guessed too low"
+    elsif @user_guess == 35
+      @message = "nice job you did the thing"
+    end
+    # return an appropriate message
+    render 'guess.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    # demo examples
     get "/query" => "params_examples#query_example"
+
+
+
+    # exercise examples
+    get "/name" => "params_examples#name"
+    get "/guess_query" => "params_examples#guess"
+    
   end
 end
