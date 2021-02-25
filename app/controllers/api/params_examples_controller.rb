de1c8848b70d5efacc7052c0333321535025a973
brian <riceb53@gmail.com>
2021-02-24 20:14:08 -0500

    add query params examples

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -1,5 +1,7 @@
 class Api::ParamsExamplesController < ApplicationController
   def query_example
+    p "HERE ARE THE PARAMS"
+    @output_message = "Here is the evil version of your name #{params[:first_name].upcase.reverse}"
     render 'query.json.jb'
   end
 end
